7f8c6420e83b9428850079b8e7b79099f7a6476b
John Deng Madit Akech <127344777+John-Akech@users.noreply.github.com>
2024-10-10 18:24:32 +0200

    Update probability.py

--- bernoulli_distribution/probability.py
+++ bernoulli_distribution/probability.py
@@ -1,7 +1,7 @@
 # Importing libraries:
 
 import numpy as np  # For numerical computations
-import matplotlib.pyplot as plt  # Foror plotting graphs
+import matplotlib.pyplot as plt  # For plotting graphs
 
 # Parameters for the Bernoulli distribution
 p = 0.60  # Probability of scoring a goal (success), e.g., a player makes a shot 60% of the time
