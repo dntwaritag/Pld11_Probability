87caccdd76199b89052471fb72747b48578899f6
Denys NTWARITAGANZWA <127384993+dntwaritag@users.noreply.github.com>
2024-10-10 18:22:12 +0200

    Update probability.py

--- bernoulli_distribution/probability.py
+++ bernoulli_distribution/probability.py
@@ -2,7 +2,6 @@
 
 import numpy as np  # For numerical computations
 import matplotlib.pyplot as plt  # Foror plotting graphs
-from scipy.stats import binom  # Importing binom from scipy for binomial distribution-related functions
 
 # Parameters for the Bernoulli distribution
 p = 0.60  # Probability of scoring a goal (success), e.g., a player makes a shot 60% of the time
